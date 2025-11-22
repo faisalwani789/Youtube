089c033bbe16df555e21e58616212f4ec4cc17c2
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-11-22 22:05:59 +0530

    added get comments hook

---
+++ src/hooks/useGetComments.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import { Youtube_comments } from '../utils/Constants'
+
+const useGetComments = (id) => {
+    console.log(id)
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        const getComments = async () => {
+            const res = await fetch(`${Youtube_comments}${id}&key=${import.meta.env.VITE_YOUTUBE_API}`)
+            const comments = await res.json()
+            setComments(comments.items)
+            
+        }
+        getComments()
+    }, [id])
+
+    return{comments}
+}
+
+export default useGetComments
