85f6d8129b58952edc7d898ab4e0f56bc397bcfc
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-11-27 14:32:53 +0530

    code cleanup

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Main from "../Components/Main"
 import Watch from "../Components/Watch";
-import LiveScrore from "../Components/LiveScrore";
+
 const router = createBrowserRouter([
     {
         path: "/",
